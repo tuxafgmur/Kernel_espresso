ba4c2f3c661713b6d3aa047b5376be386b01973d
tuxafgmur <tuxafgmur@users.noreply.github.com>
2014-08-09 03:04:35 +0200

    Fs-VFS and sync based on Jan Kara's VFS code

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2047,6 +2047,7 @@ extern void bd_set_size(struct block_device *, loff_t size);
 extern void bd_forget(struct inode *inode);
 extern void bdput(struct block_device *);
 extern void invalidate_bdev(struct block_device *);
+extern void iterate_bdevs(void (*)(struct block_device *, void *), void *);
 extern int sync_blockdev(struct block_device *bdev);
 extern struct super_block *freeze_bdev(struct block_device *);
 extern void emergency_thaw_all(void);
@@ -2065,6 +2066,9 @@ static inline struct super_block *freeze_bdev(struct block_device *sb)
 static inline int thaw_bdev(struct block_device *bdev, struct super_block *sb)
 {
 	return 0;
+}
+static inline void iterate_bdevs(void (*f)(struct block_device *, void *), void *arg)
+{
 }
 #endif
 extern int sync_filesystem(struct super_block *);
