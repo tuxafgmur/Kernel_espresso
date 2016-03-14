3c0d47b6417c5b11665aa649f8b586e00065569c
tuxafgmur <tuxafgmur@users.noreply.github.com>
2016-03-14 03:20:16 +0100

    Add Lulzactive frequency governor
    Copyright (C) 2010 Google, Inc.
    Author: Mike Chan (mike@android.com)
    Editor: Tegrak (luciferanna@gmail.com)
    Tuner : Gokhan Moral <gm@alumni.bilkent.edu.tr>
    
    This governor allows to get a full dynamic cpu frequency
    capable system for higer latency-sensitive workloads.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -364,6 +364,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
+extern struct cpufreq_governor cpufreq_gov_lulzactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lulzactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
